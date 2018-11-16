4dcfc37538af166ec5d7d3132b08d7aa61fb7d53
Oskar Schöldström <m@oxy.fi>
2018-11-16 07:33:38 -0300

    use modernizr touch detection

--- jquery.ui.touch-punch.js
+++ jquery.ui.touch-punch.js
@@ -23,8 +23,24 @@
   }
 }(function( $ ) {
 
+  function isTouchDevice() {
+    var prefixes = ' -webkit- -moz- -o- -ms- '.split(' ');
+    var mq = function(query) {
+      return window.matchMedia(query).matches;
+    }
+
+    if (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) {
+      return true;
+    }
+
+    // include the 'heartz' as a way to have a non matching MQ to help terminate the join
+    // https://git.io/vznFH
+    var query = ['(', prefixes.join('touch-enabled),('), 'heartz', ')'].join('');
+    return mq(query);
+  }
+
   // Detect touch support
-  $.support.touch = 'ontouchend' in document;
+  $.support.touch = isTouchDevice();
 
   // Ignore browsers without touch support
   if (!$.support.touch) {
